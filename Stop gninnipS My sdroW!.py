803933b948f338f8d979092e0819d121e02a82d7
Krupshark <59340146+Krupshark@users.noreply.github.com>
2022-03-27 14:47:21 +0300

    Delete Stop gninnipS My sdroW!.py

--- Stop gninnipS My sdroW!.py
+++
@@ -1,7 +0,0 @@
-def spin_words(sentence):
-    words = sentence.split(' ')
-    new_words = [w[::-1] if len(w) >= 5 else w for w in words]
-    return ' '.join(new_words)
-
-
-print(spin_words("Hey fellow warriors"))
